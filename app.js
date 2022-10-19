4634092720552eb8f58fba2be3c734e0c0083d85
vardan71 <vardanhakobyan7771@gmail.com>
2022-10-19 18:00:44 +0400

    task3

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const num1 = [1, 2, 3, 4, 6, 60];
-const num2 = [60, 7, 8, 9, 5, 11, 12, 13, 45, 6, 1];
-let result = [];
+let num = [2, 3, 8, 1, 6];
+let c;
 
-for (let i = 0; i < num1.length; i++) {
-    for (let j = 0; j < num2.length; j++) {
-        if (num1[i] == num2[j]) {
-            result.push(num2[j]);
+for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j < num.length; j++) {
+        if (num[j] > num[j + 1]) {
+            c = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = c;
         }
     }
 }
 
-console.log(result);
+console.log(num);
